41de872148d0b240dbe1af0dc4111e936c6c22db
Rodrigo Da Cruz <0001205693@senaimgaluno.com.br>
2026-05-20 14:04:19 -0300

    xanubia-corrigido prefix_adimin

--- routes/web.php
+++ routes/web.php
@@ -27,31 +27,28 @@ Route::get('/contato', function () {
     return view('contato');
 });
 
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('admin.home');
+    });
 
-Route::get('/admin', function () {
-    return view('admin.home');
+    Route::get('/index', function () {
+        return view('admin.home');
+    });
+
+    Route::get('/produtos', function () {
+        return view('admin.produtos');
+    });
+
+    Route::get('/pedidos', function () {
+        return view('admin.pedidos');
+    });
+
+    Route::get('/clientes', function () {
+        return view('admin.clientes');
+    });
+
+    Route::get('/configuracoes', function () {
+        return view('admin.configuracoes');
+    });
 });
-
-Route::get('/admin/index', function () {
-    return view('admin.home');
-});
-
-Route::get('/admin/produtos', function () {
-    return view('admin.produtos');
-
-});
-
-Route::get('/admin/pedidos', function () {
-    return view('admin.pedidos');
-});
-
-Route::get('/admin/clientes', function () {
-    return view('admin.clientes');
-});
-
-Route::get('/admin/configuracoes', function () {
-    return view('admin.configuracoes');
-});
-
-
-
